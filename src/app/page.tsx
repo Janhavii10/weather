c8394be3e8778d5cbcee097b1a9a82a3ba798df0
Janhavi Paul <janhavipaul43@gmail.com>
2024-05-08 14:09:15 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,9 @@ import axios from "axios";
 import { format, fromUnixTime, parse, parseISO } from "date-fns";
 import Image from "next/image";
 import { useQuery } from "react-query";
+import { loadingCityAtom, placeAtom } from "./atom";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
 
 
 // https://api.openweathermap.org/data/2.5/forecast?q=kolkata&appid=490e1724f014230e6fd233eb02750125&cnt=56
@@ -73,15 +76,23 @@ interface WeatherData {
 }
 
 export default function Home() {
+  const [place, setPlace] = useAtom(placeAtom);
+  const [loadingCity, ] = useAtom(loadingCityAtom);
+
+
   const { isLoading, error, data, refetch } = useQuery<WeatherData>(
     "repoData",
     async () => {
       const { data } = await axios.get(
-        "https://api.openweathermap.org/data/2.5/forecast?q=kolkata&appid=490e1724f014230e6fd233eb02750125&cnt=56"
+        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=490e1724f014230e6fd233eb02750125&cnt=56`
       );
       return data;
     }
   );
+
+  useEffect(() =>{
+    refetch();
+  }, [place, refetch])
 
   const firstData = data?.list[0];
 
@@ -114,9 +125,11 @@ export default function Home() {
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-      <Navbar />
+      <Navbar location={data?.city.name}/>
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
         {/* today data  */}
+        {loadingCity ? (<WeatherSkeleton/>) : (
+        <>
         <section className="space-y-4 ">
           <div className="space-y-2">
             <h2 className="flex gap-1 text-2xl  items-end ">
@@ -213,7 +226,49 @@ export default function Home() {
 
 
         </section>
+        </>
+        )}
       </main>
     </div>
+  );
+}
+
+function WeatherSkeleton() {
+  return (
+    <section className="space-y-8 ">
+      {/* Today's data skeleton */}
+      <div className="space-y-2 animate-pulse">
+        {/* Date skeleton */}
+        <div className="flex gap-1 text-2xl items-end ">
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+          <div className="h-6 w-24 bg-gray-300 rounded"></div>
+        </div>
+
+        {/* Time wise temperature skeleton */}
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {[1, 2, 3, 4].map((index) => (
+            <div key={index} className="flex flex-col items-center space-y-2">
+              <div className="h-6 w-16 bg-gray-300 rounded"></div>
+              <div className="h-6 w-6 bg-gray-300 rounded-full"></div>
+              <div className="h-6 w-16 bg-gray-300 rounded"></div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* 7 days forecast skeleton */}
+      <div className="flex flex-col gap-4 animate-pulse">
+        <p className="text-2xl h-8 w-36 bg-gray-300 rounded"></p>
+
+        {[1, 2, 3, 4, 5, 6, 7].map((index) => (
+          <div key={index} className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+            <div className="h-10 w-10 bg-gray-300 rounded-full"></div>
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+            <div className="h-8 w-28 bg-gray-300 rounded"></div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,15 +5,21 @@ import { MdSunny } from "react-icons/md";
 import { MdLocationOn } from "react-icons/md";
 import SearchBox from './SearchBox';
 import axios from 'axios';
-type Props = {}
+import { loadingCityAtom, placeAtom } from '@/app/atom';
+import { useAtom } from 'jotai';
+import { setTimeout } from 'timers';
 
-export default function Navbar({ }: Props) {
+type Props = { location?: string };
+
+export default function Navbar({ location }: Props) {
 
     const [city, setCity] = useState("");
     const [error, setError] = useState("");
 
     const [suggestions, setSuggestions] = useState<string[]>([]);
     const [showSuggestions, setShowSuggestions] = useState(false);
+    const [place, setPlace] = useAtom(placeAtom);
+    const [_, setLoadingCity] = useAtom(loadingCityAtom);
 
     async function handleInputChange(value: string) {
         setCity(value);
@@ -38,13 +44,19 @@ export default function Navbar({ }: Props) {
     }
 
     function handleSubmiSearch(e: React.FormEvent<HTMLFormElement>) {
+        setLoadingCity(true)
         e.preventDefault();
         if (suggestions.length == 0) {
             setError("Location not found");
-          } else {
-            setError("");
-            setShowSuggestions(false);
-          }
+            setLoadingCity(false);
+        } else {
+            setError('');
+            setTimeout(() => {
+                setLoadingCity(false);
+                setPlace(city);
+                setShowSuggestions(false);
+            }, 500);
+        }
     }
 
     function handleSuggestionClick(value: string) {
@@ -52,32 +64,69 @@ export default function Navbar({ }: Props) {
         setShowSuggestions(false);
     }
 
+    function handleCurrentLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async (position) => {
+                const { latitude, longitude } = position.coords;
+                try {
+                    setLoadingCity(true);
+                    const response = await axios.get(
+                        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=490e1724f014230e6fd233eb02750125`);
+                    setTimeout(() => {
+                        setLoadingCity(false);
+                        setPlace(response.data.name);
+                    }, 500);
+                } catch (error) {
+                    setLoadingCity(false);
+                }
+            })
+        }
+    }
+
     return (
-        <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-            <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
-                <div className='flex items-center justify-center gap-2'>
-                    <h2 className="text-gray-500 text-3xl">Weather </h2>
-                    <MdSunny className='text-3xl mt-1 text-yellow-400' />
-                </div>
-                <section className='flex gap-2 items-center '>
-                    <BiCurrentLocation className='text-2xl text-gray-400 hover-opacity-80 cursor-pointer' />
-                    <MdLocationOn className='text-3xl' />
-                    <p className='text-slate-900/80 text-sm'>India</p>
-                    <div className="relative" >
-                        <SearchBox value={city}
-                             onSubmit={handleSubmiSearch}
-                            onChange={(e) => handleInputChange(e.target.value)} />
-                        <SuggestionBox {...{
-                            showSuggestions,
-                            suggestions,
-                            handleSuggestionClick,
-                            error
-                        }} />
+        <>
+            <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+                <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+                    <div className='flex items-center justify-center gap-2'>
+                        <h2 className="text-gray-500 text-3xl">Weather </h2>
+                        <MdSunny className='text-3xl mt-1 text-yellow-400' />
                     </div>
-                </section>
-            </div>
-        </nav>
-    )
+                    <section className='flex gap-2 items-center '>
+                        <BiCurrentLocation
+                            title=' Your current loaction'
+                            onClick={handleCurrentLocation}
+                            className='text-2xl text-gray-400 hover-opacity-80 cursor-pointer' />
+                        <MdLocationOn className='text-3xl' />
+                        <p className='text-slate-900/80 text-sm'>{location}</p>
+                        <div className="relative hidden md:flex" >
+                            <SearchBox value={city}
+                                onSubmit={handleSubmiSearch}
+                                onChange={(e) => handleInputChange(e.target.value)} />
+                            <SuggestionBox {...{
+                                showSuggestions,
+                                suggestions,
+                                handleSuggestionClick,
+                                error
+                            }} />
+                        </div>
+                    </section>
+                </div>
+            </nav>
+            <section className="flex  max-w-7xl px-2 md:hidden "> {/* justify- center*/} 
+                <div className="relative " >
+                    <SearchBox value={city}
+                        onSubmit={handleSubmiSearch}
+                        onChange={(e) => handleInputChange(e.target.value)} />
+                    <SuggestionBox {...{
+                        showSuggestions,
+                        suggestions,
+                        handleSuggestionClick,
+                        error
+                    }} />
+                </div>
+            </section>
+        </>
+    );
 }
 
 function SuggestionBox({ showSuggestions,
